cb3437e2b20a295d08aa4fe912254c1e1b3d948e
Jameson Nash <vtjnash@gmail.com>
2017-04-20 13:27:14 -0400

    more bettering updates

--- contrib/julia-config.jl
+++ contrib/julia-config.jl
@@ -1,14 +1,18 @@
 #!/usr/bin/env julia
 # This file is a part of Julia. License is MIT: http://julialang.org/license
 
-const options =
-[
+const options = [
     "--cflags",
     "--ldflags",
     "--ldlibs"
 ];
 
 threadingOn() = ccall(:jl_threading_enabled, Cint, ()) != 0
+
+function shell_escape(str)
+    str = replace(str, "'", "'\''")
+    return "'$str'"
+end
 
 function imagePath()
     opts = Base.JLOptions()
@@ -23,15 +27,14 @@ function libDir()
     end
 end
 
-private_libDir() = joinpath(JULIA_HOME, Base.PRIVATE_LIBDIR)
+private_libDir() = abspath(JULIA_HOME, Base.PRIVATE_LIBDIR)
 
 function includeDir()
-    dir, bin = splitdir(JULIA_HOME)
-    return joinpath(dir, "include", "julia")
+    return abspath(JULIA_HOME, Base.INCLUDEDIR, "julia")
 end
 
 function ldflags()
-    fl = replace("""-L$(libDir())""","\\","\\\\")
+    fl = "-L$(shell_escape(libDir()))"
     if is_windows()
         fl = fl * " -Wl,--stack,8388608"
     elseif is_linux()
@@ -47,7 +50,7 @@ function ldlibs()
         "julia"
     end
     if is_unix()
-        return replace("""-Wl,-rpath,$(libDir()) -Wl,-rpath,$(private_libDir()) -l$libname""","\\","\\\\")
+        return "-Wl,-rpath,$(shell_escape(libDir())) -Wl,-rpath,$(shell_escape(private_libDir())) -l$libname"
     else
         return "-l$libname -lopenlibm"
     end
@@ -56,7 +59,7 @@ end
 function cflags()
     flags = IOBuffer()
     print(flags, "-std=gnu99")
-    include = replace(includeDir(),"\\","\\\\")
+    include = shell_escape(includeDir())
     print(flags, " -I", include)
     if threadingOn()
         print(flags, " -DJULIA_ENABLE_THREADING=1")
@@ -70,7 +73,7 @@ end
 function check_args(args)
     checked = intersect(args, options)
     if length(checked) == 0 || length(checked) != length(args)
-        println(STDERR,"Usage: julia-config [",reduce((x,y)->"$x|$y",options),"]")
+        println(STDERR, "Usage: julia-config [", join(options, " | "), "]")
         exit(1)
     end
 end
